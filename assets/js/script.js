9d09bd9a6a043c71f2daf90ca7ae7a7a0be27777
alefranii <afmormar@gmail.com>
2017-07-10 13:10:03 -0400

    mejoras, despues de arreglar el pc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,40 @@
-   $(document).ready(function(){
-                var max = 649;
-                for(var i = 1; i <= max; i++){
-                    $(".pokemon").append("<img src=\"https://pokeapi.co/media/img/" + i + ".png\" id=\"" + i + "\" class=\"pokemonImg\">");
-                };
-                
-                $(".pokemon").on("click", "img", function(e){
-                    $(".pokedexName").html("");
-                    $(".attributes").html("");
-                    $("#height").html("");
-                    $("#weight").html("");
-                    $("#types").html("");
 
-                    
-                });
-                   
-            });
+   $(document).ready(function(){
+    var max = 15;
+    for(var i = 1; i <= max; i++){
+        $(".pokemon").append("<img src=\"https://pokeapi.co/media/img/" + i + ".png\" id=\"" + i + "\" class=\"pokemonImg\">");
+    };
+    
+    $('img', this).click(function() {
+
+        var uniqueId = $(this).attr('id');
+
+        var webAddress = "http://pokeapi.co/api/v1/pokemon/" + uniqueId;
+
+        var types = " ";
+
+        $.get(webAddress, function(res) {
+            for (var j = 0; j < res.types.length; j++) {
+                types += "<li>" + res.types[j].name + "</li>";
+
+            }
+        }, 'json');
+
+        $.get(webAddress, function(res) {
+
+            $('div.pokedex').html(
+                "<h1>" + res.name + "</h1>" +
+                "<img src=http://pokeapi.co/media/img/" + uniqueId + ".png>" +
+                "<h4>Types:</h4>" +
+                "<ul>" +
+                types +
+                "</ul>" +
+                "<h4>Height:</h4>" +
+                "<p>" + res.height + "</p>" +
+                "<h4>Weight:</h4>" +
+                "<p>" + res.weight + "</p>"
+            );
+
+        }, 'json');
+    });
+});
